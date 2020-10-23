8b998f5136817c2547404e531236b0becccd1bf9
thehousealwayswins-ez <71276900+thehousealwayswins-ez@users.noreply.github.com>
2020-10-22 20:33:30 -0400

    Add files via upload

--- tutorial_8.R
+++ tutorial_8.R
@@ -1,25 +1,55 @@
 # Tutorial Week 8
-game_day <- read.table(file = "UWvMSU_1-22-13.txt", header = TRUE, sep = " ")
-game_day
 
+#import data from game day text file
+game_day <- read.table(file = "UWvMSU_1-22-13.txt", header = TRUE, sep = "\t")
 
+#create subsets for each team's score
+UWpoints <- subset(game_day, game_day$team == "UW")
+MSUpoints <- subset(game_day, game_day$team == "MSU")
+
+#now use a for loop to track cumulative scores that sums each point to the "i"th point in the loop and stores it in a vector
+
+csUW = numeric(length=length(UWpoints$score))
+for(i in 1:length(UWpoints$score)){
+  csUW[i] = sum(UWpoints$score[1:i])
+  }
+csUW
+
+csMSU = numeric(length=length(MSUpoints$score))
+for(i in 1:length(MSUpoints$score)){
+  csMSU[i] = sum(MSUpoints$score[1:i])
+}
+csMSU
+
+#plot the cumulatioive points totals as a function of time
+plot(UWpoints$time, csUW, type = "l", col = "red", xlab = "Time", ylab = "Points")
+lines(MSUpoints$time, csMSU, type = "l", col = "green", xlab = "Time", ylab = "Points")
 
 # Problem 2 - Guessing Game
 
-#utility function to set up format of game
-guess_a_number <- function()
-{
-x <- readline(prompt = "Guess a Number: ")
-n <- integer(length = 100)
-sample.int(n, size = 1, replace = FALSE)
-  return(as.integer(n))
+#generate random number from 1-100
+rand_num = sample(1:100, 1)
+
+#ask user to guess number
+print("Guess a number 1 to 100")
+
+#user can guess up to 10 times
+for(i in 1:10){
+  guess <- readline(prompt="Guess: ")
+  guess <- as.integer(guess)
+  #checks if random number is equal to guess, breaks the loop
+  if(rand_num == guess)
+    print("You got it!")
+  
+#if guess is low
+else if(guess > rand_num){
+  print("It's Lower!")
 }
 
-# if else loop 
-guess <- readinteger()
-if(guess == num)
-{cat("You got it!\n")}
-if(guess < num)
-{cat("It's Bigger!\n")}
-if(guess > num)
-{cat("It's Smaller!\n")}
+#if guess is high
+else if(guess < rand_num){
+  print("It's Higher!")
+  break
+}
+  
+}
